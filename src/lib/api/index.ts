a9d1c8a703c1b100af83e168f005e6a269a3ae0c
Nikolay Magarin <magarinnikolay@gmail.com>
2025-05-26 19:43:41 +0300

    auth fix

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import { config } from '../../config';
 import { errorHandler } from './errors';
 
 export const api = axios.create({
   baseURL: config.apiBaseUrl,
+});
+
+api.interceptors.request.use(async (reqConfig) => {
+  let token = Cookies.get(config.cookie.accessToken);
+  if (token) {
+    reqConfig.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return reqConfig;
 });
 
 api.interceptors.response.use((res) => res, errorHandler);
