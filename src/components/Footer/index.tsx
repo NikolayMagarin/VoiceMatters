6d575369d63570d2c4a40c51f5a80da3f742bb2f
Nikolay Magarin <magarinnikolay@gmail.com>
2025-05-30 12:13:53 +0300

    fill footer with links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,40 +1,31 @@
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>
-        ...
-        <br />
-        ...
-        <br />
-        ...
-        <br />
-        ...
-        <br />
-        ...
+      <div className={styles['logo-wrapper']}>
+        <img
+          className={styles.logo}
+          src='/assets/images/logo60.png'
+          alt=''
+        ></img>
+        VoiceMatters
       </div>
-      <div>
-        ...
-        <br />
-        ...
-        <br />
-        ...
-        <br />
-        ...
-        <br />
-        ...
+      <div className={styles.column}>
+        <div className={styles.name}>Документация</div>
+        <Link to='/wiki'>Пользователю</Link>
+        <Link to='/docs'>Разработчику</Link>
       </div>
-      <div>
-        ...
-        <br />
-        ...
-        <br />
-        ...
-        <br />
-        ...
-        <br />
-        ...
+      <div className={styles.column}>
+        <div className={styles.name}>О нас</div>
+        <Link to='/company'>О компании</Link>
+        <Link to='mailto:DenEKBen1@yandex.ru'>Обратная связь</Link>
+      </div>
+      <div className={styles.column}>
+        <div className={styles.name}>Помощь</div>
+        <Link to='/faq'>FAQ</Link>
+        <Link to='mailto:DenEKBen1@yandex.ru'>Сообщить об ошибке</Link>
       </div>
     </footer>
   );
